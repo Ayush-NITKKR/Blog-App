4746bdf59d2e213025fad69c9d3170ab48e7e04e
Ayush Tiwari <ayushtiwari449@gmail.com>
2026-03-26 01:56:37 +0530

    Database is connected

--- index.js
+++ index.js
@@ -8,4 +8,11 @@ const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, ()=> console.log(`server is started at PORT ${PORT}`));
 
+const connect = require('./config/connection');
+
+connect().then(()=> console.log('Mongo db is connected')).catch((err)=>{ 
+    console.log('Error connecting to database:', err)
+    process.exit(1);
+});
+
 
