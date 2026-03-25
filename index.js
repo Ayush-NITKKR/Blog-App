6f2699d9c4b0c114b816f22e5754b3f7c1dc20c3
Ayush Tiwari <ayushtiwari449@gmail.com>
2026-03-26 01:40:43 +0530

    Finally Server is started

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-console.log('hello');
-const name = 'ayush';
+const express  =  require('express');
+
+const app = express();
+
+require('dotenv').config();
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, ()=> console.log(`server is started at PORT ${PORT}`));
 
 
-function add(sum){
-    return sum;
-}
